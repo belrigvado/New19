3968ab3a9430d12ac97f28b30adadea2a493773f
alisa <alisa-12005@yandex.ru>
2023-07-15 15:28:49 +0500

    Initial commit

---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+
+        int x;
+        int a=1;
+        int b=a+1;
+        boolean r=false;
+        r = true;
+
+
+        char g = ' ';
+        float t = 4.44F;
+
+        String te;
+
+        System.out.println("Hello!");
+        System.out.println(1998);
+        System.out.println(a);
+        System.out.println(++a+1);
+        System.out.println(a+++1);
+
+
+    }
+}
